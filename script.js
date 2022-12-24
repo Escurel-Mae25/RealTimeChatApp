a5710ab4973616673cd9856a26e983d05670c9a3
Maezzzkurel <escurelmae@gmail.com>
2022-12-24 17:25:35 +0800

    user updated

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ const BOT_MSGS = [
 const BOT_IMG = "https://image.flaticon.com/icons/svg/327/327779.svg";
 const PERSON_IMG = "https://image.flaticon.com/icons/svg/145/145867.svg";
 const BOT_NAME = "BOT";
-const PERSON_NAME = "Mae Escurel";
+const PERSON_NAME = "User";
 
 msgerForm.addEventListener("submit", event => {
   event.preventDefault();
